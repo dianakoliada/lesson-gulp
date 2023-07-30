64b92637a60be1965e3163fc5323615f106d87a4
Diana Koliada <koliada.di@gmail.com>
2023-07-30 12:34:38 +0300

    created an empty shopping cart

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,30 @@ const fileinclude = require("gulp-file-include");
 
 // Задача перетворення sass на css
 gulp.task("sass", () => {
-  return gulp
-    .src("sass/*.sass")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css/"));
+    return gulp
+        .src("sass/*.sass")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("./css/"));
 });
 
 // Підключити html файли і сформувати єдиний файл
 gulp.task("html", () => {
-  return gulp
-    .src("*.html")
-    .pipe(
-      fileinclude({
-        prefix: "@@",
-        basepath: "@file",
-      })
-    )
-    .pipe(gulp.dest("html/"));
+    return gulp
+        .src("*.html")
+        .pipe(
+            fileinclude({
+                prefix: "@@",
+                basepath: "@file",
+            })
+        )
+        .pipe(gulp.dest("html/"));
 });
 
 // Слідкуємо за зміною файлів і викликаємо відповідні задачі
 gulp.task("watch", function () {
-  gulp.watch("sass/*.sass", gulp.series(["sass"]));
-  gulp.watch("*.html", gulp.series(["html"]));
-  gulp.watch("blocks/*.html", gulp.series(["html"]));
+    gulp.watch("sass/*.sass", gulp.series(["sass"]));
+    gulp.watch("*.html", gulp.series(["html"]));
+    gulp.watch("blocks/*.html", gulp.series(["html"]));
 });
 
 // Стандратна задача для виклику "gulp"
